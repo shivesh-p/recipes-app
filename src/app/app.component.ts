6c478b5f633160ddc30545f03bffefa54797b07a
shivesh-p <shiveshp530@gmail.com>
2023-03-01 10:55:58 +0530

    angular-universal-complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as AuthActions from './auth/store/actions';
 import * as appState from './store/app.reducer';
@@ -8,12 +9,21 @@ import * as appState from './store/app.reducer';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private store: Store<appState.AppState>) {}
+  constructor(
+    private store: Store<appState.AppState>,
+    @Inject(PLATFORM_ID) private platformId
+  ) {}
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     //this.authService.autoLogin();
-
-    this.store.dispatch(new AuthActions.AutoLogin());
+    /*
+      since localStorage is a browser only API so , to access localStorage in
+       auto login it will fail if server side rendering is used , we use it only 
+       when browser only angular runs.
+     */
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
   }
 }
